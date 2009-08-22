25fe7c863c2cdb359a03b30d323021d1429c337f
dandes <astrocreepz@gmail.com>
2009-08-22 19:23:57 -0400

    New main cpp and main header

---
+++ main.hpp
@@ -0,0 +1,21 @@
+#ifndef ROCK_MAIN_H
+#define ROCK_MAIN_H
+
+using namespace std;
+
+enum MenuOption{
+	MENU_SOLO_PLAY,
+	MENU_TWO_PLAYERS,
+	MENU_END,
+	MENU_INVALID
+};
+
+enum GameplayOption{
+	GAME_ROCK,
+	GAME_PAPER,
+	GAME_SCISSORS,
+	GAME_END,
+	GAME_INVALID
+};
+
+#endif
